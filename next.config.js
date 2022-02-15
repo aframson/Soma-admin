2b63705adf9c910e5a6c1fec5a6eeae30e6469c2
John Clever <jclever114@gmail.com>
2022-02-15 16:31:06 +0000

    working on sidebars

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ module.exports = {
 	reactStrictMode: true,
 	async redirects() {
 		return [
-			// {
-			// 	source: '/',
-			// 	destination: '/login',
-			// 	permanent: false,
-			// },
+			{
+				source: '/',
+				destination: '/merchant',
+				permanent: false,
+			},
 			// {
 			// 	source: '/auth/email-verify',
 			// 	destination: '/verify',
