c0a622377aff80e09d5f064c6b61424ccf0073a7
John Clever <jclever114@gmail.com>
2022-02-20 19:51:03 +0000

    update  market table

--- cards/Marketable.jsx
+++ cards/Marketable.jsx
@@ -21,7 +21,7 @@ function Marketable({ merchant }) {
 						>
 							<img
 								className={`rounded-full w-10 h-10 mr-4`}
-							
+								alt='.'
 							/>
 						</div>
 					)}
@@ -41,28 +41,28 @@ function Marketable({ merchant }) {
 					</div>
 				</div>
 			</td> */}
-			<td class='px-6 py-4 whitespace-nowrap'>
-				<div class='text-sm text-gray-900'>
+			<td className='px-6 py-4 whitespace-nowrap'>
+				<div className='text-sm text-gray-900'>
 					{merchant.email}
 				</div>
 			</td>
-			<td class='px-6 py-4 whitespace-nowrap'>
-				<div class='text-sm text-gray-900'>
+			<td className='px-6 py-4 whitespace-nowrap'>
+				<div className='text-sm text-gray-900'>
 					{merchant.establishment}
 				</div>
 			</td>
-			{/* <td class='px-6 py-4 whitespace-nowrap'>
-				<span class='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+			{/* <td className='px-6 py-4 whitespace-nowrap'>
+				<span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
 					Active
 				</span>
 			</td> */}
-			<td class='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
+			<td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
 				{merchant.telephone}
 			</td>
-			<td class='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
+			<td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
 				<a
 					href='#'
-					class='text-indigo-600 hover:text-indigo-900'
+					className='text-indigo-600 hover:text-indigo-900'
 				>
 					Edit
 				</a>
--- components/Merchant/MerchantData.jsx
+++ components/Merchant/MerchantData.jsx
@@ -31,46 +31,46 @@ export default function MerchantData() {
 			console.log(data);
 		}, [data]);
 	return (
-		<div class='flex flex-col'>
-			<div class='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-				<div class='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-					<div class='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
-						<table class='min-w-full divide-y divide-gray-200'>
-							<thead class='bg-gray-50'>
+		<div className='flex flex-col'>
+			<div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+				<div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+					<div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+						<table className='min-w-full divide-y divide-gray-200'>
+							<thead className='bg-gray-50'>
 								<tr>
 									<th
 										scope='col'
-										class='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										className='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
 									>
 										Business Name
 									</th>
 									<th
 										scope='col'
-										class='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										className='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
 									>
 										Email
 									</th>
 									<th
 										scope='col'
-										class='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										className='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
 									>
 										Establishment
 									</th>
 									<th
 										scope='col'
-										class='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										className='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
 									>
 										Phone Number
 									</th>
 									<th
 										scope='col'
-										class='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										className='px-6 py-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
 									>
 										Action
 									</th>
 								</tr>
 							</thead>
-							<tbody class='bg-white divide-y divide-gray-200'>
+							<tbody className='bg-white divide-y divide-gray-200'>
 								{data.map(merchant =>
 									<Marketable key={merchant.id} merchant={merchant}/>								
 								)}
